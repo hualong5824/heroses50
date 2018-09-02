ed53275ed3d20237a2b2d783b450c16e98228c00
hl <hualong5824@163.com>
2018-09-03 00:43:34 +0800

    配置按钮的所对应的路由与抽取组件独立创建每个按钮对应的文件夹

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,10 +1,19 @@
 <template>
           <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
-          <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+          <!-- <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
           <li><a href="#">Reports</a></li>
           <li><a href="#">Analytics</a></li>
-          <li><a href="#">Export</a></li>
+          <li><a href="#">Export</a></li> -->
+          <router-link tag="li" to="/heroes">
+          <a href="">英雄管理</a>
+          </router-link>
+          <router-link tag="li" to="/eqyips">
+          <a href="">武器管理</a>
+          </router-link>
+          <router-link tag="li" to="/weapons">
+          <a href="">装备管理</a>
+          </router-link>
         </ul>
       </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css'
 
+//导入（路由）组件
+import HerList from './views/heroes/List.vue' //英雄所对应的组件
+import WearponList from './views/weapons/List.vue' //装备所对应的组件
+import EqyipsList from './views/eqyips/List.vue' //武器所对应的组件
+// Vue.js 官方提供的一些插件 
+// (例如 vue-router) 在检测到 Vue 是可访问的全局变量时会自动调用 Vue.use()。
+// 然而在例如 CommonJS 的模块环境中，你应该始终显式地调用 Vue.use()：
+Vue.use(VueRouter);
+//创建路由对象
+const router = new VueRouter({
+    linkExactActiveClass: 'active',
+    routes: [
+        { path: '/heroes', component: HerList },
+        { path: '/eqyips', component: EqyipsList },
+        { path: '/weapons', component: WearponList },
+    ]
+})
 new Vue({
+
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
